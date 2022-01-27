433602ce39390f314a78c17d5033dbfd910b6d78
João Gabriel <main.joaogabrielsg@gmail.com>
2022-01-27 09:13:14 -0300

    feat: add code from day three project

---
+++ src/components/ChatMessageList.tsx
@@ -0,0 +1,71 @@
+import { Box, Image, Text } from "@skynexui/components";
+import { useTheme } from "styled-components";
+
+type Message = {
+  id: string;
+  from: string;
+  text: string;
+};
+
+type ChatMessageList = {
+  messages: Message[];
+};
+
+export function ChatMessageList({ messages }: ChatMessageList) {
+  const theme = useTheme();
+
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflow: "scroll",
+        display: "flex",
+        flexDirection: "column-reverse",
+        flex: 1,
+        color: theme.colors.neutrals["000"],
+        marginBottom: "16px",
+      }}
+    >
+      {messages.map((message) => (
+        <Text
+          key={message.id}
+          tag="li"
+          styleSheet={{
+            borderRadius: "5px",
+            padding: "6px",
+            marginBottom: "12px",
+            hover: {
+              backgroundColor: theme.colors.neutrals[700],
+            },
+          }}
+        >
+          <Box styleSheet={{ marginBottom: "8px" }}>
+            <Image
+              styleSheet={{
+                width: "20px",
+                height: "20px",
+                borderRadius: "50%",
+                display: "inline-block",
+                marginRight: "8px",
+              }}
+              src={`https://github.com/${message.from}.png`}
+            />
+            <Text tag="strong">{message.from}</Text>
+            <Text
+              styleSheet={{
+                fontSize: "10px",
+                marginLeft: "8px",
+                color: theme.colors.neutrals[300],
+              }}
+              tag="span"
+            >
+              {new Date().toLocaleDateString()}
+            </Text>
+          </Box>
+
+          {message.text}
+        </Text>
+      ))}
+    </Box>
+  );
+}
--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,3 +1,115 @@
+import { KeyboardEvent, useState } from "react";
+import Head from "next/head";
+import { Box, TextField } from "@skynexui/components";
+import { useTheme } from "styled-components";
+
+import { ChatHeader, ChatMessageList } from "@/components";
+
+import { appName } from "@/shared/constants";
+
+type Message = {
+  id: string;
+  from: string;
+  text: string;
+};
+
 export default function ChatPage() {
-  return <h1>Chat</h1>;
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState<Message[]>([]);
+
+  const theme = useTheme();
+
+  function createNewMessage() {
+    const newMessage = { id: "asdfhkl", from: "joaogabriel-sg", text: message };
+
+    setMessageList((prevMessageList) => [newMessage, ...prevMessageList]);
+    setMessage("");
+  }
+
+  function handleMessageKeyPress(event: KeyboardEvent<HTMLInputElement>) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      createNewMessage();
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Chat | {appName}</title>
+      </Head>
+
+      <Box
+        styleSheet={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: theme.colors.primary[500],
+          backgroundImage: `linear-gradient(-155deg, ${theme.colors.primary[500]}, ${theme.colors.primary[900]})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundBlendMode: "multiply",
+          color: theme.colors.neutrals[0],
+        }}
+      >
+        <Box
+          styleSheet={{
+            display: "flex",
+            flexDirection: "column",
+            flex: 1,
+            boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+            borderRadius: "5px",
+            backgroundColor: theme.colors.neutrals[700],
+            height: "100%",
+            maxWidth: "95%",
+            maxHeight: "95vh",
+            padding: "32px",
+          }}
+        >
+          <ChatHeader />
+
+          <Box
+            styleSheet={{
+              position: "relative",
+              display: "flex",
+              flex: 1,
+              height: "80%",
+              backgroundColor: theme.colors.neutrals[600],
+              flexDirection: "column",
+              borderRadius: "5px",
+              padding: "16px",
+            }}
+          >
+            <ChatMessageList messages={messageList} />
+
+            <Box
+              as="form"
+              styleSheet={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <TextField
+                value={message}
+                onChange={({ target }) => setMessage(target.value)}
+                onKeyPress={handleMessageKeyPress}
+                placeholder="Insira sua mensagem aqui..."
+                type="textarea"
+                styleSheet={{
+                  width: "100%",
+                  border: "0",
+                  resize: "none",
+                  borderRadius: "5px",
+                  padding: "6px 8px",
+                  backgroundColor: theme.colors.neutrals[800],
+                  marginRight: "12px",
+                  color: theme.colors.neutrals[200],
+                }}
+              />
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 }
